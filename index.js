346059f60f9b0ef15be3fa004629409d508913cf
emmaglorypraise <emmaglorypraise@gmail.com>
2020-11-02 15:13:58 +0100

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+let  Item = {
+  id: 001,
+  name: "item1",
+  price : 2000,
+  availability : true
+}
+
+console.log(Item);
+
+const arr = [Item, Item, Item, Item, Item, Item, Item, Item, Item, Item];
+
+console.log(arr);
