0ed45bf5a009f2466c64d224cdb52bd2829c3585
emmaglorypraise <emmaglorypraise@gmail.com>
2020-11-05 12:22:12 +0100

    added Id

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ could do this by toggling a class on the item.
 - A github repo showing your completed code
 - Your site hosted somewhere on the internet eg. vercel, netlify, firebase
 - This readme file updated with the approach you took and an explanation on your decisions
+
+# My approach in solving this problem
+1. I created an array of 10 objects containing keys and values of ID(number), name(string), price(number) and availability(boolean).
+2. I created a function("createItem) to display the objects in the array as items. I used the app div in the HTML file to display the contents of the array.
+3. I created a div("store") and another div("item") with document.createElement and gave them a class with .className
+4. I appended three paragraph tags(p) and a button to the item div and set their text content to the values in the array
+5. I then mapped the function to the array using .map(createItem)
+6. I then set an ID attribute to each value using set attribute to label each item in the item div
+7. I then added an event listener to the button and added a function to apply the line-through style on the button and p tags when the button is clicked
+
+Note: Getting this to work was not as easy as it seems. I had to try different things and debug a whole lot before I got it to work.
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 const catalog = [ {
   id: 1,
-  name: "01 - Item1",
+  name: "Item1",
   price: 200,
   available: false
 }, 
 {
   id: 2,
-  name: "02 - Item2",
+  name: "Item2",
   price: 300,
   available: false
 },  
 {
   id: 3,
-  name: "03 - Item3",
+  name: "Item3",
   price: 400,
   available: false
 },
 {
   id: 4,
-  name: "04 - Item4",
+  name: "Item4",
   price: 300,
   available: false
 },
 {
   id: 5,
-  name: "05 - Item5",
+  name: "Item5",
   price: 600,
   available: false
 },
 {
   id: 6,
-  name: "06 - Item6",
+  name: "Item6",
   price: 800,
   available: false
 },
 {
   id: 7,
-  name: "07 - Item7",
+  name: "Item7",
   price: 900,
   available: false
 },
 {
   id: 8,
-  name: "08 - Item8",
+  name: "Item8",
   price: 500,
   available: false
 },
 {
   id: 9,
-  name: "09 - Item9",
+  name: "Item9",
   price: 300,
   available: false
 },
 {
   id: 10,
-  name: "10 - Item10",
+  name: "Item10",
   price: 300,
   available: false
 }];
@@ -66,48 +66,49 @@ function createItem(cart) {
 
   const item = document.createElement("div");
   item.classList.add("item");
+
+  const id = document.createElement("p");
+  id.textContent = cart.id;
+  id.setAttribute("id", `id-${cart.id}`)
+
   const name = document.createElement("p");
   name.textContent = cart.name
   name.setAttribute("id", `name-${cart.id}`)
+
   const price = document.createElement("p");
   price.textContent = cart.price
   price.setAttribute("id", `price-${cart.id}`)
+
   const toggleButton = document.createElement("button");
   toggleButton.setAttribute("id", `btn-${cart.id}`)
   toggleButton.textContent = "Available";
-  const cartID = cart.id;
+  
 
   toggleButton.onclick = function(e) {
     toggleButton.classList.toggle("notAvailable")
     function strike() {
       if (toggleButton.className === "notAvailable"){
-        name.classList.toggle("notAvailable") && price.classList.toggle("notAvailable")
-        console.log("worked");
+        id.classList.toggle("notAvailable") && name.classList.toggle("notAvailable") && price.classList.toggle("notAvailable")
+        // console.log("worked");
       } else {
-        name.classList.remove("notAvailable") || price.classList.remove("notAvailable")
-        console.log("worked again");
+        id.classList.toggle("notAvailable") || name.classList.remove("notAvailable") || price.classList.remove("notAvailable")
+        // console.log("worked again");
       }
     }
     strike();
   }
-
   
+  item.appendChild(id);
   item.appendChild(name);
   item.appendChild(price);
   item.appendChild(toggleButton);
   store.appendChild(item);
   app.appendChild(store);
- 
+
   return cart
 
-  
 }
-
 
 const items = catalog.map(createItem);
 
-function changeState() {
-console.log("button worked")
-
-}
 
